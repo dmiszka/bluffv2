b15a2c00b7ed81e77e01bf0e5e55a692100d5748
Daniel Miszka <d.miszka05@gmail.com>
2024-03-04 13:46:31 +0100

    v1

--- bluff/src/tsx/Main.tsx
+++ bluff/src/tsx/Main.tsx
@@ -1,98 +1,128 @@
 import React, { useState } from 'react';
 import '../css/Main.css';
 
-const Main: React.FC = () => {
-  return (
-    <div>
-    <table className='tablica'>
-      <tbody>
-        <tr className='gorny'>
-          <td><p>11</p></td>
-          <td className="star"><p>6</p><img src="star.png"/></td>
-          <td><p>12</p></td>
-          <td><p>13</p></td>
-          <td className="star"><p>7</p><img src="star.png"/></td>
-          <td><p>14</p></td>
-          <td><p>15</p></td>
-          <td className="star"><p>8</p><img src="star.png"/></td>
-          <td><p>16</p></td>
-          <td><p>17</p></td>
-        </tr>
-        <tr>
-          <td><p>10</p></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="star"><p>9</p><img src="star.png"/></td>
-        </tr>
-        <tr>
-          <td className="star"><p>5</p><img src="star.png"/></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td><p>18</p></td>
-        </tr>
-        <tr>
-          <td><p>9</p></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td><p>19</p></td>
-        </tr>
-        <tr>
-          <td><p>8</p></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="star"><p>10</p><img src="star.png"/></td>
-        </tr>
-        <tr>
-          <td className="star"><p>4</p><img src="star.png"/></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td className="empty"></td>
-          <td><p>20</p></td>
-        </tr>
-        <tr>
-          <td><p>7</p></td>
-          <td><p>6</p></td>
-          <td className="star"><p>3</p><img src="star.png"/></td>
-          <td><p>5</p></td>
-          <td><p>4</p></td>
-          <td className="star"><p>2</p><img src="star.png"/></td>
-          <td><p>3</p></td>
-          <td><p>2</p></td>
-          <td className="star"><p>1</p><img src="star.png"/></td>
-          <td><p>1</p><img src="pawn.png"/></td>
-        </tr>
+type DieState = {
+  value: number;
+}
 
-      </tbody>
-    </table>
+const Die: React.FC<{ value: number }> = ({ value }) => {
+  return (
+    <div className="die">
+      {value}
+    </div>
+  );
+};
+
+
+const Main: React.FC = () => {
+  const [diceValues, setDiceValues] = useState<number[]>([1, 1, 1, 1, 1]);
+
+  const rollDice = () => {
+    const newDiceValues = Array.from({ length: 5 }, () => Math.floor(Math.random() * 6) + 1);
+    setDiceValues(newDiceValues);
+  };
+
+  return (
+    <div className='board'>
+
+      
+
+      <table className='tablica'>
+        <tbody>
+          <tr className='gorny'>
+            <td><p>11</p></td>
+            <td className="star"><p>6</p><img src="star.png" /></td>
+            <td><p>12</p></td>
+            <td><p>13</p></td>
+            <td className="star"><p>7</p><img src="star.png" /></td>
+            <td><p>14</p></td>
+            <td><p>15</p></td>
+            <td className="star"><p>8</p><img src="star.png" /></td>
+            <td><p>16</p></td>
+            <td><p>17</p></td>
+          </tr>
+          <tr>
+            <td><p>10</p></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="star"><p>9</p><img src="star.png" /></td>
+          </tr>
+          <tr>
+            <td className="star"><p>5</p><img src="star.png" /></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td><p>18</p></td>
+          </tr>
+          <tr>
+            <td><p>9</p></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td><p>19</p></td>
+          </tr>
+          <tr>
+            <td><p>8</p></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="star"><p>10</p><img src="star.png" /></td>
+          </tr>
+          <tr>
+            <td className="star"><p>4</p><img src="star.png" /></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td className="empty"></td>
+            <td><p>20</p></td>
+          </tr>
+          <tr>
+            <td><p>7</p></td>
+            <td><p>6</p></td>
+            <td className="star"><p>3</p><img src="star.png" /></td>
+            <td><p>5</p></td>
+            <td><p>4</p></td>
+            <td className="star"><p>2</p><img src="star.png" /></td>
+            <td><p>3</p></td>
+            <td><p>2</p></td>
+            <td className="star"><p>1</p><img src="star.png" /></td>
+            <td><p>1</p><img src="pawn.png" /></td>
+          </tr>
+
+        </tbody>
+      </table>
+      <h1>Rzut pięcioma kostkami</h1>
+      <div className="dice-container">
+        {diceValues.map((value, index) => (
+          <Die key={index} value={value} />
+        ))}
+      </div>
+      <button onClick={rollDice} className='btn-bg'>Rzuć kostkami</button>
     </div>
   );
 };
